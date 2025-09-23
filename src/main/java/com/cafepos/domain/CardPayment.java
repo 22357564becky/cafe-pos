a00cc6e0002a27956deac68a67b7d63835f3623f
22357564becky <beckymeade2745@gmail.com>
2025-09-23 11:35:47 +0100

    Added Test and Demo files

---
+++ src/main/java/com/cafepos/domain/CardPayment.java
@@ -0,0 +1,23 @@
+package com.cafepos.domain;
+
+public final class CardPayment implements PaymentStrategy {
+
+    private final String cardNumber;
+
+    public CardPayment(String cardNumber) {
+        if (cardNumber == null || cardNumber.length() < 4) {
+            throw new IllegalArgumentException("Card number must be at least 4 digits long.");
+        }
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public void pay(Order order) {
+
+        String lastFour = cardNumber.substring(cardNumber.length() - 4);
+        String maskedCard = "****" + lastFour;
+
+        System.out.println("[Card] Customer paid " + order.totalWithTax(10) + " EUR with card " + maskedCard);
+    }
+}
+
