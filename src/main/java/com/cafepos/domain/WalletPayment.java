1c706dd6eef4d9e9f8b3b0f3da3cb6e5faed7b1d
maevebray <22345744@studentmail.ul.ie>
2025-09-23 11:44:16 +0100

    edits to CashPayment and walletPayment to update the print statements to match the new format.

--- src/main/java/com/cafepos/domain/WalletPayment.java
+++ src/main/java/com/cafepos/domain/WalletPayment.java
@@ -1,14 +1,16 @@
 package com.cafepos.domain;
 
-public final class WalletPayment {
+public final class WalletPayment implements PaymentStrategy {
     private final String walletId;
 
     public WalletPayment(String walletId) {
         this.walletId = walletId;
     }
 
+    @Override    
     public void pay(Order order) {
-        System.out.println("Paid " + order.totalWithTax(10) + " using wallet " + walletId + " for order " + order.id());
+        System.out.println("[Wallet] Customer paid " +
+order.totalWithTax(10) + " EUR via wallet " + walletId);    
     }
     
 }
